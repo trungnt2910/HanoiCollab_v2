715555d2d61bd325cd938d4c3bda220e1e1581f3
Trung <57174311+trungnt2910@users.noreply.github.com>
2022-03-09 22:30:20 +0700

    feat: Added taskbar

--- Clients/HanoiCollab_v2.user.js
+++ Clients/HanoiCollab_v2.user.js
@@ -2258,6 +2258,73 @@ function SetupTrackingPrevention()
     }
 }
 
+function SetupTaskbar()
+{
+    var d = new Date(Date.now());
+    var parser = new DOMParser();
+    var elem = parser.parseFromString(
+        `  
+<div id="hanoicollab-fake-taskbar" style="bottom:0;right:0;left:0;position:fixed;height:48px;background:rgb(227,238,249);margin:0;z-index=9999;">
+    <img style="height:48px;position:absolute;left:0;" src="https://raw.githubusercontent.com/trungnt2910/HanoiCollab_v2/master/Clients/Images/IconsWithEdge.png"></img>
+    <img style="height:48px;position:absolute;right:0;" src="https://raw.githubusercontent.com/trungnt2910/HanoiCollab_v2/master/Clients/Images/IconsRight.png"></img>
+    <div id="hanoicollab-fake-taskbar-time" style="background:rgb(227,238,249);font-family: 'Segoe UI', Arial, sans-serif;font-size: 12.5px;text-align: right;height:48;display:flex;justify-content:right;align-items:center;position:absolute;right:47.5px;top:0;margin:0">
+        <p>
+            <span>${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}</span>
+            <br/>
+            <span>${d.getDate().toString().padStart(2, "0")}/${d.getMonth().toString().padStart(2, "0")}/${d.getFullYear()}</span>
+        </p>
+    </div>
+    <hr style="height:1px;position:absolute;top:0;width:100%;border-width:0px;background-color:rgb(213,223,233);margin:0;padding:0"></hr>
+</div>
+        `,"text/html").body.firstChild;
+    // Taskbar element should be unaffected by stealth mode.
+    elem.style.display = "none";
+    // And only allow for top iframes. Google Forms hosted in Quilgo should not listen.
+    if (window == top)
+    {
+        HanoiCollabGlobals.Document.body.appendChild(elem);
+        // flex -> none.
+        HanoiCollab$("#hanoicollab-fake-taskbar-time").toggle();
+        var taskbarDisplayed = false;
+        HanoiCollabGlobals.Document.addEventListener("keydown", function(e)
+        {
+            if (e.altKey && e.key === 't')
+            {
+                taskbarDisplayed = !taskbarDisplayed;
+                HanoiCollab$("#hanoicollab-fake-taskbar-time").toggle();
+                HanoiCollab$("#hanoicollab-fake-taskbar").toggle();
+                if (taskbarDisplayed)
+                {
+                    var style = HanoiCollabGlobals.Document.createElement("style");
+                    style.innerHTML = `
+                    ::-webkit-scrollbar {
+                        display: none;
+                    }
+                    `;
+                    style.id = "hanoicollab-hide-scrollbar-style";
+                    HanoiCollabGlobals.Document.head.appendChild(style);
+                }
+                else
+                {
+                    HanoiCollabGlobals.Document.getElementById("hanoicollab-hide-scrollbar-style").remove();
+                }
+            }
+        });
+        setInterval(function()
+        {
+            var d = new Date(Date.now());
+            HanoiCollab$("#hanoicollab-fake-taskbar-time").innerHTML = 
+            `
+            <p>
+                <span>${d.getHours().toString().padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}</span>
+                <br/>
+                <span>${d.getDate().toString().padStart(2, "0")}/${d.getMonth().toString().padStart(2, "0")}/${d.getFullYear()}</span>
+            </p>
+            `
+        }, 1000);
+    }
+}
+
 (async function() 
 {
     if (window.location.origin.startsWith("blob"))
@@ -2295,6 +2362,7 @@ function SetupTrackingPrevention()
     await SetupStealthMode();
     SetupKeyBindings();
     SetupStyles();
+    SetupTaskbar();
     await SetupServer();
     await SetupIdentity();
     await SetupChatConnection();
